13a1ce4da21d68f69d3987082fc855ae5f92b118
Everaldino Oliveira dos Santos <juniornerv@hotmail.com>
2020-01-20 01:58:04 -0300

    adicionar campos áreas relacionadas

--- src/Lists/RegistroDeOcorrencia/RegistroOcorrenciaCustomForm.aspx
+++ src/Lists/RegistroDeOcorrencia/RegistroOcorrenciaCustomForm.aspx
@@ -14,11 +14,13 @@
 <asp:Content ContentPlaceHolderId="PlaceHolderMain" runat="server">
 <SharePoint:UIVersionedContent UIVersion="4" runat="server">
 	<ContentTemplate>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">		
 		<link rel="stylesheet" type="text/css" href="../../SiteAssets/css/styleform.css">
 
 		<!-- Adicionar JQuery e Testar CrossBrowser -->
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>		
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.8.2/umd/popper.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>		
 		<script src="../../SiteAssets/js/react.production.min.js"></script>
 		<script src="../../SiteAssets/js/react-dom.production.min.js"></script>
 
--- src/SiteAssets/js/registro.js
+++ src/SiteAssets/js/registro.js
@@ -1,3 +1,7 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
 async function getChoice(Select){
 
    return $.ajax({
@@ -50,19 +54,55 @@ async function getChoice(Select){
         listItems
         );
     }
- 
+
+    // Implementar depois
+    function ConfirmDeleteArea(props) {
+        return (
+            <div class="modal fade" id="deleteArea" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Remover Área Causadora</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Deseja Deletar Área Causadora {props.index}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Salvar</button>
+                </div>
+                </div>
+            </div>
+            </div>
+        )
+    }
+
     function AreaCausadora(props){
-        console.log('Areas: ', props);
         const listCausa = props.values.slice(0).reverse().map((data, index) =>
             <tr key={index.toString()}>
                 <td colspan="3">
-                    <table>
+                    <span class="arrow_box">
+                        <i onClick={(e) => props.toggle(index, e)} 
+                        className={"arrow " + (props.show[index] ? 'down' : 'right')}
+                        ></i>
+                        AreaCasudora{data}:
+
+                        {data !== 1 &&
+                            <i onClick={(e) => props.delete(data, e)}
+                            class="eraser"
+                            >x</i>
+                        }
+                    </span>
+                    <table className={(props.show[index] ? 'show' : 'hidden')}>
                             <tr>
                                 <td valign="top" class="ms-formlabel">
                                     <h3 class="ms-standardheader">
                                         <nobr>AreaCasudora {data}:</nobr>
                                     </h3>
-                                    <select required>
+                                    <select required name="Area"
+                                    onChange={props.field}
+                                    >
                                         <option value=""></option>
                                         <AreasDeReporte values={props.area} />
                                     </select>
@@ -71,7 +111,9 @@ async function getChoice(Select){
                                     <h3 class="ms-standardheader">
                                         <nobr>EmpresaCausadora {data}:</nobr>
                                     </h3>
-                                    <select required>
+                                    <select required 
+                                    // ref={props.field.textInput}
+                                    >
                                         <option value=""></option>
                                         <AreasDeReporte values={props.empresa} />
                                     </select>
@@ -82,7 +124,9 @@ async function getChoice(Select){
                                 <h3 class="ms-standardheader">
                                         <nobr>Causa {data}:</nobr>
                                     </h3>
-                                    <textarea row="6"></textarea>
+                                    <textarea row="6"
+                                    // ref={props.field.textInput}
+                                    ></textarea>
                                 </td>
                             </tr>
                     </table>
@@ -99,6 +143,9 @@ class Registro extends React.Component {
       super(props);
       this.handleSubmit = this.handleSubmit.bind(this);
       this.addCausa = this.addCausa.bind(this);
+      this.deleteCausa = this.deleteCausa.bind(this);
+      this.toggleTable = this.toggleTable.bind(this);
+      this.getValueFieldsArea = this.getValueFieldsArea.bind(this);
       this.fields = {};
       this.fields.AreaDeReporte = React.createRef();
       this.fields.EmpresaDeReporte = React.createRef();
@@ -110,11 +157,14 @@ class Registro extends React.Component {
       this.fields.Impacto = React.createRef();
       this.fields.AreasRelacionadas = React.createRef();
       this.fields.ComentariosGerais = React.createRef();
+
       this.state = {
           Areas: [],
           Empresas: [],
           Impactos: [],
           Causas: [1],
+          showItems: [],
+          fieldsArea: []
         };
     }
 
@@ -137,18 +187,6 @@ class Registro extends React.Component {
         });
     }
 
-    addCausa(event) {
-        const that = this;
-
-        console.log('Causa Adicionada: ');
-
-        this.setState((state, props) => ({
-            Causas: state.Causas.concat(state.Causas.length + 1)
-        }));
- 
-        event.preventDefault();
-    }
-
     handleSubmit(event) {
         let payload = {};
 
@@ -161,8 +199,50 @@ class Registro extends React.Component {
       event.preventDefault();
     }
 
+    toggleTable(index) {
+        let showItems = this.state.showItems.slice(0);
+            showItems[index] = !showItems[index];
+
+            this.setState({showItems});
+
+        event.preventDefault();
+    }
+
+   addCausa(event) {
+        this.setState((state, props) => ({
+            Causas: state.Causas.concat(state.Causas.length + 1)
+        }));
+ 
+        event.preventDefault();
+    }
+
+    deleteCausa(index) {
+        this.setState((state, props) => ({
+            Causas: state.Causas.filter((item, j) => index !== item)
+        }));        
+
+        event.preventDefault();
+    }
+
+    getValueFieldsArea(event) {
+        const nameField = event.target.name
+        const obj = {
+            [nameField]: event.target.value,
+        }
+
+        this.setState((state, props) => ({
+            fieldsArea: state.fieldsArea.concat(obj)
+        }));
+
+        setTimeout(() => {
+            console.log('state: ', this.state.fieldsArea);
+        },100)        
+    }
+
+
     render() {
       return (
+
         <form onSubmit={this.handleSubmit}>       
             <tr>
                 <td valign="top" class="ms-formlabel">
@@ -205,7 +285,14 @@ class Registro extends React.Component {
             <tr>
                 <td colspan="3"> <span class="icon_plus" onClick={this.addCausa}>+</span></td>
             </tr>
-                <AreaCausadora values={this.state.Causas} area={this.state.Areas} empresa={this.state.Empresas} />
+                <AreaCausadora 
+                values={this.state.Causas} 
+                area={this.state.Areas} 
+                toggle={this.toggleTable} 
+                delete={this.deleteCausa} 
+                show={this.state.showItems} 
+                field={this.getValueFieldsArea}
+                empresa={this.state.Empresas} />
             <tr>
                 <td valign="top" class="ms-formlabel">
                     <h3 class="ms-standardheader">
